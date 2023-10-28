836d17f340b73bf98411306f601c06be140a8108
sumisera <149242931+sumisera@users.noreply.github.com>
2023-10-28 16:50:08 +0100

    our proj

---
+++ OUR PROJECTT/OUR PROJECTT/Form1.cs
@@ -0,0 +1,17 @@
+namespace OUR_PROJECTT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.ControlBox = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
